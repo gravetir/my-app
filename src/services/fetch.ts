635abc39ef1be6739c0af9d5862381077cf971b7
gravetir <dimadt505228@gmail.com>
2024-05-22 16:42:59 +0300

    FixAnother

--- src/services/fetch.ts
+++ src/services/fetch.ts
@@ -1,7 +1,7 @@
 import { Category } from '../types/category'
 import { Product } from '../types/product'
 
-const BASE = 'http://localhost:8888/api/'
+const BASE = 'http://178.154.206.159:8888/api/'
 
 export async function fetchCategories(): Promise<Category[]> {
     const res = await fetch(`${BASE}category/all`)
